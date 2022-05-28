2616b4ede60da7df6faea0910b6be2dbd519270d
Justyna <j.matejkowska@gmail.com>
2022-05-28 11:40:20 +0200

    dodano funkcje liczaca polaczenia

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+class Polaczenia:
+  def __init__(self, nazwa_pliku):
+    self.nazwa_pliku = nazwa_pliku
+    self.dane_słownik = self.dane()
+
+  def dane(self):
+    kom_sl = dict()
+    with open(self.nazwa_pliku, 'r') as fin:
+      reader = csv.reader(fin, delimiter = ",")
+      naglowki = next(reader)
+
+      for row in reader:
+        dzwoniacy = int(row[0])
+        if dzwoniacy not in kom_sl:
+          kom_sl[dzwoniacy] = 0
+        kom_sl[dzwoniacy] += 1
+    return kom_sl
+
+  def pobierz_najczesciej_dzwoniacego(self):
+    return max(self.dane_słownik.items(), key= lambda x: x[1])
 
 if __name__ == "__main__":
-    pass
+  nazwa_pliku = input()
+  mp = Polaczenia(input)
+  wynik = mp.pobierz_najczesciej_dzwoniacego()
+
+  print (wynik)
